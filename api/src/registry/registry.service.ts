0745ec531b51924ca9e8aee39a4c7a4b9b8cf4ed
lucasfishh <arkmmhompulvia@hotmail.com>
2025-12-25 19:58:24 +0000

    Tighten reputation scoring for agent records

--- api/src/registry/registry.service.ts
+++ api/src/registry/registry.service.ts
@@ -3,3 +3,8 @@ import { DbService } from '../db/db.service';
 import { CONFIG } from '../config';
 import { normalizeName, isValidName } from '../common/name.util';
 
+export interface AgentRow {
+  name: string;
+  owner: string;
+  resolver: string;
+  metadata_uri: string;
