1e10d64b9af39df05c8fe999d1cf3a55281ed837
lucasfishh <arkmmhompulvia@hotmail.com>
2026-02-17 15:25:07 +0000

    Add runtime config and API client boot

--- web/src/main.js
+++ web/src/main.js
@@ -3,3 +3,18 @@ import { Buffer } from 'buffer';
 window.Buffer = window.Buffer || Buffer;
 window.global = window.global || window;
 
+import { route, initRouter } from './router.js';
+import { createNavbar } from './components/navbar.js';
+import { loadConfig } from './config.js';
+import { walletService } from './lib/wallet.js';
+
+import { landingPage } from './pages/landing.js';
+import { explorePage } from './pages/explore.js';
+import { agentPage } from './pages/agent.js';
+import { registerPage } from './pages/register.js';
+import { activityPage } from './pages/activity.js';
+import { leaderboardPage } from './pages/leaderboard.js';
+
+document.body.prepend(createNavbar());
+
+route('/', landingPage);
